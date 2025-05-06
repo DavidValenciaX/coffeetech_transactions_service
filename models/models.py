7efaaf0ccf4ebdfaac6de07986545bf54c79fc4f
DavidValencia526 <davidvalencia0526@gmail.com>
2025-05-06 00:43:11 -0500

    refactor: clean up whitespace and improve relationship definitions in models

--- models/models.py
+++ models/models.py
@@ -9,42 +9,33 @@ class TransactionStates(Base):
     transaction_state_id = Column(Integer, primary_key=True)
     name = Column(String(45), nullable=False, unique=True)
     transactions = relationship("Transactions", back_populates="state")
-    
+
 class TransactionTypes(Base):
     __tablename__ = 'transaction_types'
-
     transaction_type_id = Column(Integer, primary_key=True)
     name = Column(String(255), nullable=False, unique=True)
-
-    # Relaciones
     categories = relationship("TransactionCategories", back_populates="transaction_type")
 
 class TransactionCategories(Base):
     __tablename__ = 'transaction_categories'
     __table_args__ = (UniqueConstraint('name', 'transaction_type_id'),)
-
     transaction_category_id = Column(Integer, primary_key=True)
     name = Column(String(255), nullable=False)
-    transaction_type_id = Column(Integer, ForeignKey('transaction_types.transaction_type_id'), nullable=False)
-
-    # Relaciones
+    transaction_type_id = Column(Integer, ForeignKey('transaction_types.transaction_type_id', ondelete="CASCADE"), nullable=False)
     transaction_type = relationship("TransactionTypes", back_populates="categories")
     transactions = relationship("Transactions", back_populates="transaction_category")
 
 class Transactions(Base):
     __tablename__ = 'transactions'
-
     transaction_id = Column(Integer, primary_key=True)
-    plot_id = Column(Integer, ForeignKey('plots.plot_id'), nullable=False)
     description = Column(String(255), nullable=True)
+    entity_type = Column(String(50), nullable=False)
+    entity_id = Column(Integer, nullable=False)
     transaction_date = Column(Date, nullable=False)
     transaction_state_id = Column(Integer, ForeignKey('transaction_states.transaction_state_id'), nullable=False)
     value = Column(Numeric(15, 2), nullable=False)
     transaction_category_id = Column(Integer, ForeignKey('transaction_categories.transaction_category_id'), nullable=False)
-    creator_id = Column(Integer, ForeignKey('users.user_id'), nullable=False)
-
+    creator_id = Column(Integer, nullable=False)
     # Relaciones
-    plot = relationship("Plots", back_populates="transactions")
     transaction_category = relationship("TransactionCategories", back_populates="transactions")
     state = relationship("TransactionStates", back_populates="transactions")
-    creator = relationship("Users", back_populates="created_transactions")
